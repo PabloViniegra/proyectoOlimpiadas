ee7bc1ec9a62f693a24e9144a337e71993801b69
Pablo <pablo.viniegra@JUANXXIII.net>
2020-02-21 17:28:11 +0100

    Implementada segunda opcion del menu, sacada de polla

--- Proyecto Final/JavaApplication2/src/controlador/Main.java
+++ Proyecto Final/JavaApplication2/src/controlador/Main.java
@@ -20,11 +20,8 @@ public class Main {
     public static void main(String[] args) {
         
         Formulario f1 = new Formulario();
-        boolean validacion = false;
-        while (!validacion) {
-            f1.menu();
-            validacion= true;
-        }
+        f1.menu();
+            
             
         
         
--- Proyecto Final/JavaApplication2/src/vista/Formulario.java
+++ Proyecto Final/JavaApplication2/src/vista/Formulario.java
@@ -7,13 +7,17 @@ package vista;
 
 import controlador.GestionFicheros;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import modelo.Deportista;
 import modelo.Medalla;
 import modelo.Medalla.Tipo;
@@ -29,7 +33,7 @@ public class Formulario {
         Formulario f1 = new Formulario();
         Deportista d1 = new Deportista();
         Medalla m1 = new Medalla();
-        byte numusuario=0;
+        byte numusuario = 0;
         do {
             System.out.println("________MENÚ________");
             System.out.println("");
@@ -40,28 +44,29 @@ public class Formulario {
             System.out.println("____________________");
             System.out.print("Para elegir una opción introduzca su número"
                     + " correspondiente: ");
-            boolean validacion = true;
+            boolean validacion = false;
             while (!validacion) {
                 try {
                     numusuario = sc.nextByte();
                     validacion = true;
                 } catch (InputMismatchException e) {
                     System.out.println("Introduce el número de la opción del"
-                            + " menú");
+                            + " menú");                    
+                } finally{
                     sc.nextLine();
                 }
             }
-            
             switch (numusuario) {
+                
                 case 1:
-                    Deportista miDeportista = new Deportista();
-                    Formulario miFormulario = new Formulario();
-                    
-                    miDeportista=miFormulario.pideDatosDeportista();
+                    d1=f1.pideDatosDeportista();
                     System.out.println("Deportista agregado con éxito");
                     break;
                 case 2:
-                    System.out.println("Lorem Ipsum");
+                    System.out.print("Introduzca nombre del deportista a "
+                            + "consultar: ");
+                    String nombre = sc.nextLine();
+                    f1.consultaDeportista(nombre);
                     break;
                 case 3:
                     System.out.println("Lorem Ipsum");
@@ -137,7 +142,7 @@ public class Formulario {
                 sc.nextLine();
             }
         }
-        System.out.print("¿Qué tipo de medalla es? Marque su numero");
+        System.out.println("¿Qué tipo de medalla es? Marque su numero");
         System.out.println("1. Oro");
         System.out.println("2. Plata");
         System.out.println("3. Bronce");
@@ -178,31 +183,46 @@ public class Formulario {
         return m1;
     }
     
-    public void consultaDeportista(Deportista d, File lectura) {
-        
+    public void consultaDeportista(String deportista) {
+        Deportista temporal;
+        deportista=deportista.concat(".olimpiadas");
+        File lectura = new File (deportista);
+        FileInputStream canal = null;
         try {
             Scanner teclado = new Scanner(System.in);
-            System.out.print("Escribe el nombre del deportista que"
-                    + " quieres consultar: ");
-            String respuesta = teclado.nextLine();
-            
-            if (respuesta.equalsIgnoreCase(d.getNombre())) {
-                Scanner sc = new Scanner(lectura);
-                while (sc.hasNextLine()) {
-                    String data = sc.nextLine();
-                    System.out.println(data);
-                }
-                sc.close();
+            canal = new FileInputStream(lectura);
+            ObjectInputStream ficheroleido = new ObjectInputStream(canal);
+            File f = new File(deportista);
+            if (f.exists()) {
+                temporal = (Deportista) ficheroleido.readObject();
+                System.out.println("Nombre: " + temporal.getNombre());
+                System.out.println("Pais: " + temporal.getPais());
+                System.out.println("Número de medallas: "
+                        + temporal.getNumMedallas());
+                System.out.println("Medallas: " + temporal.getArrMedalla());
+                
             } else {
                 System.out.println("No se ha encontrado el nombre del"
                         + " Deportista. Prueba otra vez");
                 Formulario formrecursive = new Formulario();
-                formrecursive.consultaDeportista(d, lectura);
+                formrecursive.consultaDeportista(deportista);
             }
             
         } catch (FileNotFoundException e) {
             System.out.println("No se encuentra el archivo. Quizás no exista "
                     + "en el directorio");
+        } catch (IOException e) {
+            System.out.println("Ha ocurrido un error de tipo IOException");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Ha ocurrido un error de tipo"
+                    + " ClassNotFoundException");
+        } finally {
+            try {
+                canal.close();
+            } catch (IOException e) {
+                System.out.println("Error de tipo IOException al cerrar el"
+                        + " fichero");
+            }
         }
         
     }
