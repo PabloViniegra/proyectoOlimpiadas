bac1f70fdeb6d454e97f7ec7091df7a372b3d821
PabloViniegra <57683831+PabloViniegra@users.noreply.github.com>
2020-02-25 18:13:20 +0100

    añadido javadoc y .jar

--- Proyecto Final/JavaApplication2/src/controlador/GestionFicheros.java
+++ Proyecto Final/JavaApplication2/src/controlador/GestionFicheros.java
@@ -15,8 +15,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import modelo.Deportista;
 import modelo.Medalla;
 import vista.Formulario;
